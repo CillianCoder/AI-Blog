3754485622b2d98e6381cdb5822421193743376c
Madushan Sandakelum <75947205+CillianCoder@users.noreply.github.com>
2026-03-30 16:17:49 +0530

    Update rss-check.js

--- rss-check.js
+++ rss-check.js
@@ -18,7 +18,58 @@ const rssFeeds = [
   "https://hauntedplaces.org/feed",
   "https://charleyross.wordpress.com/feed",
   "https://storiesoftheunsolved.com/feed",
-  "https://morbidology.com/feed"
+  "https://morbidology.com/feed",
+    "https://www.truecrimedaily.com",
+  "https://listverse.com",
+  "https://radaronline.com",
+  "https://murdermap.co.uk",
+  "https://truecrimeforensics.com",
+  "https://crimerocket.com",
+  "https://truecrimestoryblog.com",
+  "https://truecrimereport.news.blog",
+  "https://truecrime.blog",
+  "https://unsolved.com",
+  "https://the-line-up.com",
+  "https://mysterydelver.com",
+  "https://rss.nytimes.com/services/xml/rss/nyt/US.xml",
+
+  // --- Working Paranormal & Mystery Feeds ---
+  "https://blog.world-mysteries.com",
+  "https://anomalien.com",
+  "https://ghosttheory.com",
+  "https://southernmostghosts.com",
+  "https://connectparanormal.net",
+  "https://paranormal-evidence.com",
+  "https://hauntedplaces.org",
+
+  // --- Working Reddit Mystery Feeds (Requires User-Agent) ---
+  "https://www.reddit.com",
+  "https://www.reddit.com",
+  "https://www.reddit.com",
+  "https://www.reddit.com",
+  "https://www.reddit.com",
+  "https://www.reddit.com",
+  "https://www.reddit.com",
+  "https://www.reddit.com",
+
+  // --- Verified New High-Quality Sources (Fixed URLs) ---
+  "https://projectcoldcase.org",
+  "https://charleyross.wordpress.com", 
+  "https://www.oxygen.com",
+  "https://defrostingcoldcases.com",
+  "https://insightcrime.org",
+  "https://www.fbi.gov",
+  "https://forensicfilesnow.com",
+  "https://crimereads.com",
+  "https://storiesoftheunsolved.com",
+  "https://morbidology.com",
+  "https://atavist.com",
+  "https://thesuitcasemurder.com",
+  "https://investigative-reporter.com",
+  "https://crimeblogger19.com",
+  "https://www.thetruecrimemuseum.co.uk",
+  "https://caughtoffguard.org",
+  "https://truecrimenews.com"
 ];
 
 (async () => {
